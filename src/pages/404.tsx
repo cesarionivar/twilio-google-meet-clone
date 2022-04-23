671c98c71d585b430b4f40002128c0555cae1430
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-23 15:10:11 -0400

    chore: style not found (404) page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 export const NotFound = () => {
   const navigate = useNavigate();
   return (
-    <div className='mt-32 text-center space-y-14'>
+    <div className='mt-32 text-center select-none space-y-14 '>
       <h1 className='text-4xl text-gray-900'>Invalid video call name.</h1>
       <button
         className='px-6 text-white bg-blue-500 rounded py-1.5 hover:bg-blue-600'
