a94fa5d9d4a4de8eb6e8a0c4ff17e907450154e7
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-23 20:03:35 -0400

    chore(bug): add local tracks

--- src/pages/PreJoining.tsx
+++ src/pages/PreJoining.tsx
@@ -9,19 +9,30 @@ export const PreJoining = () => {
     useJoinRoom();
   const videoDiv = useRef<HTMLDivElement | null>(null);
 
+  const clearVideoDiv = () => {
+    if (videoDiv.current?.firstElementChild) {
+      videoDiv.current?.removeChild(videoDiv.current.firstElementChild);
+    }
+  };
+
   useEffect(() => {
-    console.log('Generated the video with the new config');
+    clearVideoDiv();
 
     createLocalTracks({ audio: withAudio, video: withVideo }).then(
       (localTracks) => {
         localTracks.forEach((track: any) => {
+          if (!withVideo) {
+            track.stop();
+            return;
+          }
+
           videoDiv.current?.appendChild(track.attach());
         });
       }
     );
 
     return () => {
-      videoDiv.current = null;
+      clearVideoDiv();
     };
   }, [withAudio, withVideo]);
 
@@ -29,25 +40,27 @@ export const PreJoining = () => {
     <div>
       <Navbar />
       <h2>Pre-join Room</h2>
-      <div ref={videoDiv} />
-      <button
-        onClick={toggleAudio}
-        className='bg-slate-800 p-5 mx-4 text-white rounded'
-      >
-        Audio
-      </button>
-      <button
-        onClick={toggleVideo}
-        className='bg-slate-800 p-5 mx-4 text-white rounded'
-      >
-        Video
-      </button>
-      <button
-        onClick={joinRoom}
-        className='bg-slate-800 p-5 mx-4 text-white rounded'
-      >
-        Join Room
-      </button>
+      <div ref={videoDiv} className='w-80 h-80' />
+      <div className='absolute top-4 right-4'>
+        <button
+          onClick={toggleAudio}
+          className='bg-slate-800 p-5 mx-4 text-white rounded'
+        >
+          Audio
+        </button>
+        <button
+          onClick={toggleVideo}
+          className='bg-slate-800 p-5 mx-4 text-white rounded'
+        >
+          Video
+        </button>
+        <button
+          onClick={joinRoom}
+          className='bg-slate-800 p-5 mx-4 text-white rounded'
+        >
+          Join Room
+        </button>
+      </div>
     </div>
   );
 };
